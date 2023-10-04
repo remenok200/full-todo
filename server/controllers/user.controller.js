64d1b67815944097ab8e5d8181ade73b7ef37196
Roman Ratushnyi <freshcode1001@ukr.net>
2023-10-04 19:51:42 +0300

    Login + password hash

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,11 @@
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 module.exports.registatrionUser = async (req, res, next) => {
     try {
-        const {body} = req;
+        const {body, passwordHash} = req;
 
-        const createdUser = await User.create(body);
+        const createdUser = await User.create({...body, passwordHash});
 
         return res.status(201).send(createdUser);
     } catch (error) {
@@ -13,5 +14,19 @@ module.exports.registatrionUser = async (req, res, next) => {
 }
 
 module.exports.loginUser = async (req, res, next) => {
-    
+    try {
+        const { body } = req;
+        const foundUser = await User.findOne({
+            email: body.email
+        });
+        if(foundUser) {
+            const result = await bcrypt.compare(body.password, foundUser.passwordHash);
+            if(!result) {
+                return next(new Error());
+            }
+            return res.status(200).send('Logged in');
+        }
+    } catch (error) {
+        next(error);
+    }
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 const UserController = require('../controllers/user.controller');
+const {hashPass} = require('../middlewares/hashPassword');
 
 const userRouter = Router();
 
-userRouter.post('/sign-up', UserController.registatrionUser); // register
+userRouter.post('/sign-up', hashPass, UserController.registatrionUser); // register
 userRouter.post('/sign-in', UserController.loginUser); // login
 
 module.exports = userRouter;
