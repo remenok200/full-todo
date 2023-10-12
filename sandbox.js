ce109bb05c5865496c3a725bc5ee7e4bc634c570
Roman Ratushnyi <freshcode1001@ukr.net>
2023-10-12 19:23:14 +0300

    Generators

--- sandbox.js
+++ sandbox.js
@@ -22,3 +22,136 @@ console.log(iterator.next()); // { value: undefined, done: true }
 for(const num of numbers) {
     console.log(num);
 }
+
+// Задача: Реалізувати кастомний ітератор
+
+const myIterator = {
+    data: [10, 20, 30, 40, 50],
+    currentIndex: 0,
+    next() {
+        if(this.currentIndex < this.data.length) {
+            return {
+                value: this.data[this.currentIndex++],
+                done: false
+            }
+        } else {
+            return {
+                value: undefined,
+                done: true
+            }
+        }
+    }
+};
+
+console.log(myIterator.next()); // { value: 10, done: false }
+console.log(myIterator.next()); // { value: 20, done: false }
+console.log(myIterator.next()); // { value: 30, done: false }
+console.log(myIterator.next()); // { value: 40, done: false }
+console.log(myIterator.next()); // { value: 50, done: false }
+console.log(myIterator.next()); // { value: undefined, done: true }
+
+
+// Задача: створити об'єкт та перебрати його ітератором
+
+const myObject = {
+    name: 'John',
+    lastName: 'Doe',
+    age: 44,
+    city: 'Kyiv'
+};
+
+// Створюємо кастомний ітератор під об'єкти
+
+const myObjectIterator = {
+    currentKey: Object.keys(myObject)[0],
+    next() {
+        const keys = Object.keys(myObject);
+        const currentIndex = keys.indexOf(this.currentKey) === -1 ? keys.length + 1 : keys.indexOf(this.currentKey);
+        if(currentIndex < keys.length) {
+            const Key = keys[currentIndex];
+            this.currentKey = keys[currentIndex + 1];
+            return {
+                value: myObject[Key],
+                done: false
+            }
+        }
+        return {
+            value: undefined,
+            done: true
+        }
+    }
+};
+
+console.log(myObjectIterator.next()); // { value: 'John', done: false }
+console.log(myObjectIterator.next()); // { value: 'Doe', done: false }
+console.log(myObjectIterator.next()); // { value: '44', done: false }
+console.log(myObjectIterator.next()); // { value: 'Kyiv', done: false }
+console.log(myObjectIterator.next()); // { value: undefined, done: true }
+
+
+// Генератори
+
+function* myGenerator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const it = myGenerator();
+
+console.log(it.next()); // {value: 1, done: false}
+console.log(it.next()); // {value: 2, done: false}
+console.log(it.next()); // {value: 3, done: false}
+console.log(it.next()); // {value: undefined, done: true}
+
+// Задача: написати функцію-генератор, яка приймає початок і кінець.
+// Далі, в циклі, йде від початку і до кінця, через yield виводить поточне значення (проміжний результат)
+
+function* mySecondGenerator(start, end) {
+    for(let i = start; i <= end; i++) {
+        yield i;
+    }
+}
+
+const it2 = mySecondGenerator(1, 3);
+
+console.log(it2.next()); // {value: 1, done: false}
+console.log(it2.next()); // {value: 2, done: false}
+console.log(it2.next()); // {value: 3, done: false}
+console.log(it2.next()); // {value: undefined, done: true}
+
+
+
+// Задача: написати функцію-генератор, яка буде вітатись з користувачем.
+// При тому, імені користувача при запуску генератора у нас ще не буде
+// Ім'я користувача з'являється ПІСЛЯ запуску генератора
+
+
+function* myThirdGenerator() {
+    let username = yield;
+    console.log(`Hi ${username}`);
+}
+
+const it3 = myThirdGenerator();
+
+it3.next(); // запуск генератора
+
+it3.next('John Doe'); // передаємо значення генератору
+
+// Асинхронна робота з використанням генераторів
+
+function* fetchUrl(url) {
+    try {
+        const response = yield fetch(url);
+        const data = yield response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const it4 = fetchUrl('https://randomuser.me/api/');
+
+it4.next().value
+.then(response => it4.next(response).value)
+.then(data => console.log(data));
