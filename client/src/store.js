527047450db7e26ff17e5db4dce4c77016a19770
Roman Ratushnyi <freshcode1001@ukr.net>
2023-10-17 20:44:32 +0300

    devTools enhancer

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,12 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import rootSaga from './sagas';
 import reducer from './reducer';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const enhancer = applyMiddleware(sagaMiddleware);
+const enhancer = composeWithDevTools(applyMiddleware(sagaMiddleware));
 
 const store = createStore(reducer, enhancer);
 
