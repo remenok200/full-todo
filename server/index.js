fdd63d1fc514df4c417f0aef24bf55d03532519d
Roman Ratushnyi <freshcode1001@ukr.net>
2023-10-27 18:34:20 +0300

    Websocket notification server side done

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 const http = require('http');
+const  { Server } = require("socket.io");
 const app = require('./app');
+
+const cors = {
+    origin: '*'
+}
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+const io = new Server(server, cors);
+
+io.on('connect', (socket) => {
+    console.log('CONNECTION!');
+
+    // задача, кожні 5 секунд відправляти push-повідомлення на клієнт
+    setTimeout(() => {
+        io.emit('NEW_NOTIFICATION', {notification: 'Something new happened'});
+    }, 5000);
+
+    socket.on('disconnect', (reason) => {
+        console.log(reason);
+    })
+})
 
 server.listen(PORT, () => {
     console.log(`App started on port ${PORT}`);
